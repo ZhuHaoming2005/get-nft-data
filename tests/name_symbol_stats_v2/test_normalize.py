40c343ddfd14b7d972021ab13cdaad17da0453f3
ZhuHaoming2005 <m17663905966@163.com>
2026-04-01 23:06:50 +0800

    feat: add adaptive blocking primitives

--- tests/name_symbol_stats_v2/test_normalize.py
+++ tests/name_symbol_stats_v2/test_normalize.py
@@ -1,9 +1,10 @@
-﻿import unittest
+import unittest
 
 from name_symbol_stats_v2.normalize import (
     build_name_block_key,
     build_name_signature,
     build_name_signature_hash,
+    collapse_name_for_blocking,
     normalize_name,
     normalize_symbol,
 )
@@ -13,6 +14,11 @@ class NormalizeTests(unittest.TestCase):
     def test_normalize_name_strips_numeric_suffix(self):
         self.assertEqual(normalize_name('CryptoPunks #123'), 'cryptopunks')
         self.assertEqual(normalize_name('CryptoPunks #456'), 'cryptopunks')
+
+    def test_collapse_name_for_blocking_removes_separators(self):
+        self.assertEqual(collapse_name_for_blocking(normalize_name('Andy Duboc')), 'andyduboc')
+        self.assertEqual(collapse_name_for_blocking(normalize_name('andy-duboc')), 'andyduboc')
+        self.assertEqual(collapse_name_for_blocking(normalize_name('andy_duboc')), 'andyduboc')
 
     def test_normalize_symbol_collapses_space(self):
         self.assertEqual(normalize_symbol(' A Z U K I '), 'azuki')
